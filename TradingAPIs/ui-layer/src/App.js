3471f3f2bd3efccbfc0a3f46c8330749cf2e20b2
SahasraSynergy <connect@sahasrasynergy.com>
2025-01-04 17:15:38 +0530

    changes for build fix

--- TradingAPIs/ui-layer/src/App.js
+++ TradingAPIs/ui-layer/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import ProductList from './ProductList';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AnnouncementsList from "./AnnouncementsList";
@@ -8,14 +7,12 @@ import AddAnnouncement from "./AddAnnouncement";
 function App() {
   return (
     <div className="App">
-     
-          {/* <ProductList /> */}
           <Router>
-    <Routes>
-      <Route path="/" element={<AnnouncementsList />} />
-      <Route path="/add-announcement" element={<AddAnnouncement />} />
-    </Routes>
-    </Router>
+            <Routes>
+              <Route path="/" element={<AnnouncementsList />} />
+              <Route path="/add-announcement" element={<AddAnnouncement />} />
+            </Routes>
+          </Router>
     </div>
   );
 }
